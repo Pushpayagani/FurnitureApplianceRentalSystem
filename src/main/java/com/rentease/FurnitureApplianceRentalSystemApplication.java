f3c7ed860d0d9c70353fa0bd67561c5db4152d88
Pushpayagani <yaganipushpa48@gmail.com>
2026-03-01 16:06:26 +0530

    pushing code

---
+++ src/main/java/com/rentease/FurnitureApplianceRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rentease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureApplianceRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureApplianceRentalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentease/controller/FurnitureApplianceRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentease.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureApplianceRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
